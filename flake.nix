d7c9fabdc01ad52b2cab45ef73b653ccf0d134c8
Trey Fortmuller <tfortmuller@anduril.com>
2024-02-17 01:02:54 +0000

    add a flake

--- flake.nix
+++ flake.nix
@@ -1,18 +1,27 @@
-+{
-+  description = "The missile knows where it is.";
-+
-+  inputs = {
-+    # NixOS official package source, using the nixos-22.11 branch here
-+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-+  };
-+
-+  outputs = { self, nixpkgs, ... }@inputs: {
-+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-+      system = "x86_64-linux";
-+      modules = [
-+        ./msi_trident/configuration.nix
-+      ];
-+      specialArgs = { inherit inputs; };
-+    };
-+  };
-+}
+{
+  description = "The missile knows where it is.";
+
+  inputs = {
+    # NixOS official package source, using the nixos-22.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+        kearsarge = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./msi_trident/configuration.nix
+          ];
+          specialArgs = { inherit inputs; };
+        };
+
+        # TODO... more of them
+    };
+
+    nixosModules = {
+      # The base configuration to be dependended on private machines
+      base = import ./base-configuration.nix;
+    };
+  };
+}
