d9768914647dba192c2fa9048ad7481f4091ee2e
Trey Fortmuller <tfortmuller@mac.com>
2024-03-13 15:44:52 +0000

    add in git recent

--- base.nix
+++ base.nix
@@ -189,20 +189,20 @@ in
           # List aliases
           la = "!git config --list | grep -E '^alias' | cut -c 7-";
 
-          last = "log -1 HEAD";
-          unstage = "reset HEAD --";
-
           # Beautiful one-liner log, last 20 commits
           l = "log --pretty=\"%C(Yellow)%h  %C(reset)%ad (%C(Green)%cr%C(reset))%x09 %C(Cyan)%an %C(reset)%s\" --date=short -20";
 
+          # Most recently checked-out branches
+          recent = "!git reflog show --pretty=format:'%gs ~ %gd' --date=relative | grep 'checkout:' | grep -oE '[^ ]+ ~ .*' | awk -F~ '!seen[$1]++' | head -n 10 | awk -F' ~ HEAD@{' '{printf(\"  \\033[33m%s: \\033[37m %s\\033[0m\\n\", substr($2, 1, length($2)-1), $1)}'";
+          
+          last = "log -1 HEAD";
+          unstage = "reset HEAD --";
           b = "branch --show";
           a = "add";
           c = "commit";
           s = "status -s";
           co = "checkout";
           cob = "checkout -b";
-
-          # TODO (tff): get git recent in here
         };
 
         extraConfig = {
@@ -488,12 +488,11 @@ in
       remmina
       feh
       git-lfs
+      sshping
+      nethogs
 
       # TODO - this thing kinda sucks, replace with feh
       setroot
-
-      sshping
-      nethogs
     ];
 
     fonts.fonts = with pkgs; [ jetbrains-mono ];
