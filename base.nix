8e5b34a0299703ed1e038ac6f5eb43fb62c9e627
Trey Fortmuller <tfortmuller@mac.com>
2024-06-21 10:13:37 +0100

    some virtualisation stuff for testing

--- base.nix
+++ base.nix
@@ -207,31 +207,32 @@ in
       };
     };
 
-    # TODO (tff): could add a virtualization module and factor this out, 
-    # https://nixos.wiki/wiki/NixOS:nixos-rebuild_build-vm
+    # TODO (tff): just go headless for our VM variant, sway doesn't launch anyway.
     # 
+    # TODO (tff): could add a virtualization module and factor this out,
+    # https://nixos.wiki/wiki/NixOS:nixos-rebuild_build-vm
+    #
     # Only added when building a VM with build-vm, this is basically a specialisation as far as I can tell.
     virtualisation.vmVariant = {
       # the flake way of running this against the local checkout of nixpkgs would be:
       # nixos-rebuild build-vm --override-input nixpkgs /home/trey/sources/nixpkgs --flake .#
-      # 
+      #
       # For testing VMs via 'nixos-rebuild build-vm'
+      #
+      # I prefix the qemu launch script with this to port forward SSH so I can
+      # hop onto the VM machine, I wonder if there's a declarative way to do this.
+      # QEMU_NET_OPTS="hostfwd=tcp::2222-:22"
       users.users.nixosvmtest = {
-        isSystemUser = true;
+        isNormalUser = true;
         initialHashedPassword = "$y$j9T$hI1mt7owhTIAMIWDChCPx.$bzOYaXaKO7B7kSewE2o88iyxVhFqb38aHz2OuZmcUS8";
         group = "nixosvmtest";
-        
-        # I prefix the qemu launch script with this to port forward SSH so I can
-        # hop onto the VM machine, I wonder if there's a declarative way to do this.
-        # QEMU_NET_OPTS="hostfwd=tcp::2222-:22"
-        useDefaultShell = true;
       };
 
       users.groups.nixosvmtest = { };
-      
+
       virtualisation = {
         memorySize =  2048; # Use 2048MiB memory.
-        cores = 3;         
+        cores = 3;
       };
     };
 
