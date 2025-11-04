c2c04928c6e086092272bf753e524201adcf7639
Trey Fortmuller <tfortmuller@mac.com>
2025-11-03 20:30:00 -0800

    custom overlay of bambu-studio

--- flake.nix
+++ flake.nix
@@ -18,13 +18,12 @@
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      nixpkgs-unstable,
-      home-manager,
-      nixpkgs-wayland,
-      ...
+    { self
+    , nixpkgs
+    , nixpkgs-unstable
+    , home-manager
+    , nixpkgs-wayland
+    , ...
     }@inputs:
     {
       nixosConfigurations = {
@@ -73,6 +72,36 @@
                 unstable = import nixpkgs-unstable {
                   system = final.system;
                   config.allowUnfree = true;
+                };
+
+                # See https://github.com/NixOS/nixpkgs/issues/440951 for bambu-studio, was running into
+                # crashes using networking features in bambu-studio. 25.05's derivation builds it from source
+                # whereas this uses the appimage and is a more recent version of the slicer.
+                bambu-studio = prev.appimageTools.wrapType2 rec {
+                  name = "BambuStudio";
+                  pname = "bambu-studio";
+                  version = "02.03.00.70";
+                  ubuntu_version = "24.04_PR-8184";
+
+                  src = prev.fetchurl {
+                    url = "https://github.com/bambulab/BambuStudio/releases/download/v${version}/Bambu_Studio_ubuntu-${ubuntu_version}.AppImage";
+                    sha256 = "sha256:60ef861e204e7d6da518619bd7b7c5ab2ae2a1bd9a5fb79d10b7c4495f73b172";
+                  };
+
+                  profile = ''
+                    export SSL_CERT_FILE="${prev.cacert}/etc/ssl/certs/ca-bundle.crt"
+                    export GIO_MODULE_DIR="${prev.glib-networking}/lib/gio/modules/"
+                  '';
+
+                  extraPkgs = pkgs: with pkgs; [
+                    cacert
+                    glib
+                    glib-networking
+                    gst_all_1.gst-plugins-bad
+                    gst_all_1.gst-plugins-base
+                    gst_all_1.gst-plugins-good
+                    webkitgtk_4_1
+                  ];
                 };
               })
 
